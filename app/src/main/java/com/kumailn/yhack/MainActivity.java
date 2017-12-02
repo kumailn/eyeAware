ca7bc6214456e6c546e952a8f6a290ffd8292532
Kumail Naqvi <kumailmn@gmail.com>
2017-12-01 22:33:26 -0500

    added Camera function

--- app/src/main/java/com/kumailn/yhack/MainActivity.java
+++ app/src/main/java/com/kumailn/yhack/MainActivity.java
@@ -1,13 +1,79 @@
 package com.kumailn.yhack;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.androidhiddencamera.CameraConfig;
+import com.androidhiddencamera.HiddenCameraActivity;
+import com.androidhiddencamera.config.CameraFacing;
+import com.androidhiddencamera.config.CameraImageFormat;
+import com.androidhiddencamera.config.CameraResolution;
+import com.androidhiddencamera.config.CameraRotation;
+
+import java.io.File;
+
+public class MainActivity extends HiddenCameraActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button n = findViewById(R.id.button);
+
+        //Setting camera configuration
+        CameraConfig mCameraConfig = new CameraConfig()
+                .getBuilder(getApplicationContext())
+                .setCameraFacing(CameraFacing.REAR_FACING_CAMERA)
+                .setCameraResolution(CameraResolution.MEDIUM_RESOLUTION)
+                .setImageFormat(CameraImageFormat.FORMAT_JPEG)
+                .setImageRotation(CameraRotation.ROTATION_270)
+                .build();
+
+        //Check for the camera permission for the runtime
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+            //Start camera preview
+            startCamera(mCameraConfig);
+        } else {
+            ActivityCompat.requestPermissions((Activity) getApplicationContext(), new String[]{Manifest.permission.CAMERA}, 101);
+        }
+
+
+        n.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onImageCapture(@NonNull File imageFile) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        //Display the image to the image view
+        ((ImageView) findViewById(R.id.cam_prev)).setImageBitmap(bitmap);
+
+
+    }
+
+    @Override
+    public void onCameraError(int errorCode) {
+
     }
 }
